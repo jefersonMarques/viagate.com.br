9536c0e0f2461764dd45d29f0f8fdb59c1d8cbef
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-09-02 09:13:40 -0300

    feat: add Google Tag Manager configuration

--- web/components/google.go
+++ web/components/google.go
@@ -5,10 +5,18 @@ import (
 	"strings"
 )
 
+func googleTagManagerID() string {
+	return strings.TrimSpace(os.Getenv("GOOGLE_TAG_MANAGER_ID"))
+}
+
 func googleAnalyticsID() string {
 	return strings.TrimSpace(os.Getenv("GOOGLE_ANALYTICS_ID"))
 }
 
 func googleSiteVerification() string {
 	return strings.TrimSpace(os.Getenv("GOOGLE_SITE_VERIFICATION"))
+}
+
+func googleAnalyticsDirectEnabled() bool {
+	return googleTagManagerID() == "" && googleAnalyticsID() != ""
 }
