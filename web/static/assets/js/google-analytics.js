8247a17b07bc018b458c14626148e820ba0b777a
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-09-02 09:05:29 -0300

    refactor: load Google tag from local initializer

--- web/static/assets/js/google-analytics.js
+++ web/static/assets/js/google-analytics.js
@@ -11,6 +11,11 @@
     window.dataLayer.push(arguments);
   };
 
+  const googleTag = document.createElement("script");
+  googleTag.async = true;
+  googleTag.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(measurementID)}`;
+  document.head.appendChild(googleTag);
+
   window.gtag("js", new Date());
   window.gtag("config", measurementID);
 })();
